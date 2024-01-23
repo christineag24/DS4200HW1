230433c3a5a31f2a5ac2f29ad0e6f50532403520
christineag24 <128718343+christineag24@users.noreply.github.com>
2024-01-22 22:29:27 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function funFact() {
+    document.getElementById("funFactButton").innerHTML = "Fun Fact: I am from Virginia!";
+  }
+
+function show() {
+    document.getElementById('image2').style.display = "block";
+    document.getElementById('imageButton').style.display = "none";
+}
